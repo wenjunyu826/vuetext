7a363bb3c4e9beedb63fb783aa0b4ed4ba4ba664
wenjunyu826 <1280872029@qq.com>
2025-10-20 11:37:45 +0800

    10.20

--- src/components/shijianchuancan.vue
+++ src/components/shijianchuancan.vue
@@ -3,6 +3,8 @@
         <h3>事件传参</h3>
         <button @click="count1">add 454</button>
         <p>count is {{ countNum }}</p>
+        <button @click="newCount('hello syxy')">newCount 222</button>
+        <hr>
     </div>
 
 </template>
@@ -12,7 +14,8 @@ export default {
     name: 'shijianchuancan',
     data() {
         return {
-            countNum:0
+            countNum:0,
+            newCountNum:20
         }
     },
     methods: {
